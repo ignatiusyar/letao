11f1a064cbfa55cf2edf3cedad2ebaa984f55ee3
ignace <ignace@live.cn>
2018-11-21 15:07:16 +0800

    登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -29,6 +29,9 @@ $(function(){
                     min: 2,
                     max: 6,
                     message: '用户名长度必须在2到6之间'
+                },
+                callback:{
+                    message:"用户名错误"
                 }
             }
         },
@@ -42,6 +45,9 @@ $(function(){
                     min: 6,
                     max: 12,
                     message: '密码长度必须在6到12之间'
+                },
+                callback:{
+                    message:"密码错误"
                 }
             }
         }
@@ -61,10 +67,10 @@ $(function(){
                 location.href = "index.html";
             }
             if( info.error === 1000){
-                alert( info.message );
+                $("#form").data("bootstrapValidator").updateStatus("username", "INVALID", "callback")
             }
             if( info.error === 1001){
-                alert( info.message );
+                $("#form").data("bootstrapValidator").updateStatus("username", "INVALID", "callback")
             }
         }
     })
